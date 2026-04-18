08c630069a17308b56fd77f560887f7824eb9fec
Win122333 <artem.chulkov.90@mail.ru>
2026-04-18 21:48:42 +0300

    init AiService

---
+++ AiService/src/main/java/ru/vsu/cs/fitAssistant/controller/AiController.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.fitAssistant.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/ai")
+public class AiController {
+    @PostMapping("/generate")
+    public ResponseEntity<List<AiResponse>>
+}
